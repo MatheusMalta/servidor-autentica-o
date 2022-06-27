23cd52ff10f842acd754ecf758f16dc1f6240600
Matheus Malta <matheus.malta@academico.ifpb.edu.br>
2022-06-27 17:15:37 -0300

    função show

--- Front-end/js/index.js
+++ Front-end/js/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const show = require('./lib/show.js');
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -11,7 +14,13 @@ app.get('/users', (req, res) => {
   res.send('Online Users');
 });
 
+app.get('/showusers', async (req, res) => {
+  const id = Number(req.params.id);
+
+  const { times } = await show();
+
+  res.json({ times });
+});
 
 
 app.listen(port)
-
